b6e01aece31226306afdd07747b9a28676acc5db
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-03-23 15:00:59 +0100

    [Controller & Templates](Character) Création d'un formulaire pour les builds communautaires et sa vue

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -62,6 +62,69 @@ class CharacterController extends AbstractController
             'officialBuilds' => $officialBuilds
         ]);
     }
+
+
+    /**
+     * @Route("/character/{id}/community-build/index", name="community-build")
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function communityBuildIndex(Request $request, int $id): Response
+    {
+        $character = $this->entityManager->getRepository(Character::class)->find($id);
+
+        /* @var Character $character */
+        $allCommunityBuild = $this->entityManager->getRepository(Build::class)->findBy(['gameCharacter' => $character->getId(), 'buildCategory' => 'COMMUNITY']);
+
+        return $this->render('character/form/index.community-build.html.twig', [
+            'charactersBuild' => $allCommunityBuild,
+            'character' => $character
+        ]);
+    }
+
+
+    /**
+     * @Route("/character/{id}/community-build/new", name="community-build-new")
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function newCommunityBuild(Request $request, int $id): Response
+    {
+        $character = $this->entityManager->getRepository(Character::class)->find($id);
+        /* @var Character $character */
+
+        $build = new Build();
+        $build->setGameCharacter($character)
+            ->setBuildCategory('COMMUNITY');
+
+        $form = $this->createFormBuilder($build)
+            ->add('name', \Symfony\Component\Form\Extension\Core\Type\TextType::class)
+            ->add('description', TextareaType::class)
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            $build = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($build);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('character', ['id' => $character->getId()]);
+        }
+
+        return $this->render('character/form/new.community-build.html.twig', [
+            'character' => $character,
+            'new' => $form->createView()
+        ]);
+    }
+
+
     /**
      * Returns all characters from the database
      *
