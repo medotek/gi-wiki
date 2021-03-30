a99ca5e71188b1c5b566fc75d3b71b385837afa4
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-03-30 13:07:30 +0200

    [Extra](ApiService) Added a validator for ajax/xmlhttp request

---
+++ src/Extra/ApiService.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\Extra;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ApiService
+{
+    private ValidatorInterface $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function validateAndCreate($data, $entityClassName){
+
+        $objectNormalizer = new ObjectNormalizer();
+        $normalizers = [$objectNormalizer];
+        $encoders = [new JsonEncoder()];
+        $serializer = new Serializer($normalizers, $encoders);
+
+        $result = $serializer->deserialize($data, $entityClassName, 'json');
+        $errors = $this->validator->validate($result);
+
+        if(count($errors) > 0){
+            throw new Exception(Response::HTTP_BAD_REQUEST, (string) $errors);
+        }
+
+        return $result;
+
+    }
+
+}
