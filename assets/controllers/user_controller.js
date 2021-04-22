ee28432e340f8ee3f1a0cd5283a96395783af6a5
Thomas Huynh <thomas.huynh@etu.univ-grenoble-alpes.fr>
2021-04-22 21:21:44 +0200

    [Assets/js](User) Added isotope configuration for the characters and get data-characters

--- assets/controllers/user_controller.js
+++ assets/controllers/user_controller.js
@@ -1,7 +1,20 @@
 import '../styles/controllers/user.scss';
 
+var Isotope = require('isotope-layout');
+
+require('isotope-cells-by-row');
+
+// make Isotope a jQuery plugin
+jQueryBridget('isotope', Isotope, $);
+
 $(document).ready(function () {
 
+    const $grid = $('.iudCharacters').isotope({
+        // main isotope options
+        itemSelector: '.user-character',
+        // set layoutMode
+        layoutMode: 'vertical'
+    });
     /*AJAX LOADER*/
 
     $("#user-build-remove").click(function () {
@@ -34,7 +47,7 @@ $(document).ready(function () {
                 url: '/account/profile/set/uid',
                 dataType: 'json',
                 data: JSON.stringify(data),
-                beforeSend: function() {
+                beforeSend: function () {
                     $("#wait").show();
                 },
                 success: function (data) {
@@ -50,46 +63,112 @@ $(document).ready(function () {
 
             var $form = $(this).closest('form');
             $form.toggleClass('is-readonly is-editing');
-            var isReadonly  = $form.hasClass('is-readonly');
+            var isReadonly = $form.hasClass('is-readonly');
             $form.find('#uid').prop('disabled', isReadonly);
         } else {
             var $form = $(this).closest('form');
             $form.toggleClass('is-readonly is-editing');
-            var isReadonly  = $form.hasClass('is-readonly');
+            var isReadonly = $form.hasClass('is-readonly');
             $form.find('#uid').prop('disabled', isReadonly);
             return false;
         }
     })
 
 
-    $('.js-edit').on('click', function() {
+    $('.js-edit').on('click', function () {
         $('.js-cancel').css({
                 display: 'inline-block'
             }
         )
         var $form = $(this).closest('form');
         $form.toggleClass('is-readonly is-editing');
-        var isReadonly  = $form.hasClass('is-readonly');
+        var isReadonly = $form.hasClass('is-readonly');
         $form.find('#uid').prop('disabled', isReadonly);
     });
 
-    $('.js-save').on('click', function() {
+    $('.js-save').on('click', function () {
         $('.js-cancel').css({
                 display: 'none'
             }
         )
     })
 
-    $('.js-cancel').on('click', function() {
+    $('.js-cancel').on('click', function () {
         $('.js-cancel').css({
                 display: 'none'
             }
         )
         var $form = $(this).closest('form');
         $form.toggleClass('is-readonly is-editing');
-        var isReadonly  = $form.hasClass('is-readonly');
+        var isReadonly = $form.hasClass('is-readonly');
         $form.find('#uid').prop('disabled', isReadonly);
     });
 
+    $('.gridFilter').click(function () {
+        $('.horizontalFilter').removeClass("filterActive");
+        $(this).addClass("filterActive");
+
+        $('.user-profile .section-user-builds #uidProfile .iudCharacters .user-character').css({
+            'display': 'block',
+            'width': 'auto',
+            'max-height': 'inherit',
+            'min-height': 'inherit',
+            'border-radius': '5px',
+            'padding': '5px',
+            'margin': '15px'
+        });
+        $('.character-constellations, .character-weapon, .character-artifacts, .sets-effect, .character-name').css({
+            'display': 'none',
+        });
+        $grid.isotope({
+            layoutMode: 'cellsByRow',
+            itemSelector: '.user-character',
+            masonry: {
+                isFitWidth: true
+            }
+        })
+    });
+
+
+    $('.horizontalFilter').click(function () {
+        $('.gridFilter').removeClass("filterActive");
+        $(this).addClass("filterActive");
+
+        const characterDatas = $('.iudCharacters.user-character');
+        characterDatas.each(function () {
+            console.log(characterDatas);
+        })
+
+        $('.character-constellations, .character-weapon, .character-artifacts, .sets-effect, .character-name').css({
+            'display': '',
+        });
+
+        $('.user-profile .section-user-builds #uidProfile .iudCharacters .user-character').css({
+            'display': '',
+            'width': '',
+            'max-height': '',
+            'min-height': '',
+            'border-radius': '',
+            'padding': ''
+        });
+
+        $grid.isotope({
+            layoutMode: 'vertical',
+            itemSelector: '.user-character',
+            masonry: {
+                isFitWidth: true
+            }
+        })
+
+
+    });
+
+    $('#user-character > .jsCharacterData').each(function(index, item) {
+        const characterData = $(item).data('characters');
+        $(item).parent().on('mouseenter', function() {
+            // console.log((characterData));
+        });
+    });
 
 });
+
